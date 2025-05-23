631f6d038c023d9c998f9898ed03e189b3698f5e
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-05-23 21:12:39 +0200

    Remaking da map

--- PlataformasUnity2D/Assets/Scripts/Player/PlayerHealth.cs
+++ PlataformasUnity2D/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -6,6 +7,11 @@ public class PlayerHealth : MonoBehaviour
     private int currentLives;
 
     public Transform spawnPoint;
+    public SpriteRenderer spriteRenderer;
+    public Color damageColor = Color.red;
+    public float flashDuration = 0.2f;
+    public AudioClip hitSound;
+    public AudioSource audioSource;
 
     void Start()
     {
@@ -18,10 +24,25 @@ public class PlayerHealth : MonoBehaviour
         currentLives = Mathf.Max(currentLives, 0);
         Debug.Log("Vidas restantes: " + currentLives);
 
+        // Reproducir sonido
+        if (audioSource != null && hitSound != null)
+            audioSource.PlayOneShot(hitSound);
+
+        // Flash rojo
+        if (spriteRenderer != null)
+            StartCoroutine(DamageFlash());
+
         if (currentLives <= 0)
         {
             Respawn();
         }
+    }
+
+    private IEnumerator DamageFlash()
+    {
+        spriteRenderer.color = damageColor;
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = Color.white;
     }
 
     private void Respawn()
