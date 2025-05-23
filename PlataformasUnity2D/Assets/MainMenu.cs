95b8bcc8c39e9c6c7645b2fdaa7c9ec51b642b25
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-05-23 19:19:26 +0200

    Acabamos menus

--- PlataformasUnity2D/Assets/MainMenu.cs
+++ PlataformasUnity2D/Assets/MainMenu.cs
@@ -3,10 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string sceneToLoad = "Level_Tutorial"; 
+    public string sceneToLoad = "Level_Tutorial";
+    public string creditsScene = "CreditsScene";
 
     public void StartGame()
     {
+        Debug.Log("Boton Presinado");
         SceneManager.LoadScene(sceneToLoad);
+    }
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(creditsScene);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
     }
 }
